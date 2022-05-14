64d3feabbaabad92af2f53e7e4238e23768f1d78
Tóth Bence 🐻 <toth.bence@simonyi.bme.hu>
2022-05-14 16:04:43 +0200

    send message if rig is online

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,65 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+import moment from "moment";
 
+dotenv.config();
 const ETHERMINE_API = "https://api.ethermine.org";
 const ETH_WALLET_ADDREESS = "23A24646fEF17a3E43fAbdec6526483452c2FF2D";
 const ONE_ETH_IN_WEI = 1000000000000000000;
-dotenv.config();
+const TELEGRAM_BOT = new Telegraf(process.env.TELEGRAM_BOT_TOKEN ?? "");
+const TELEGRAM_CHAT_ID = -632117764;
 
-console.log("Hello World");
+console.log("Bot started");
+TELEGRAM_BOT.telegram.sendMessage(TELEGRAM_CHAT_ID, `Bot started`);
 
-/* axios
-  .get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/workers`)
-  .then((res) => {
-    console.log(res.data.data);
-  })
-  .catch((error) => {
-    console.error(error);
+let downAlertSended = false;
+
+setInterval(async () => {
+  console.log("Checking workers and payouts");
+  Promise.all([
+    axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/dashboard`),
+    axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/workers`),
+    axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/payouts`),
+  ]).then(([dashboardResp, workersResp, payoutsResp]) => {
+    const activeWorkers =
+      dashboardResp.data.data.currentStatistics.activeWorkers;
+
+    if (activeWorkers < workersResp.data.data.length && !downAlertSended) {
+      TELEGRAM_BOT.telegram.sendMessage(TELEGRAM_CHAT_ID, "Rig is down!");
+      TELEGRAM_BOT.telegram.sendAnimation(
+        TELEGRAM_CHAT_ID,
+        "https://media.giphy.com/media/eImrJKnOmuBDmqXNUj/giphy.gif"
+      );
+      downAlertSended = true;
+    } else if (
+      activeWorkers === workersResp.data.data.length &&
+      downAlertSended
+    ) {
+      TELEGRAM_BOT.telegram.sendMessage(TELEGRAM_CHAT_ID, "Rig is up!");
+      TELEGRAM_BOT.telegram.sendAnimation(
+        TELEGRAM_CHAT_ID,
+        "https://media.giphy.com/media/3ohzdIuqJoo8QdKlnW/giphy.gif"
+      );
+      downAlertSended = false;
+    }
+
+    const nowDate = moment(new Date(Date.now()));
+    const lastPayoutDate = moment(
+      new Date(payoutsResp.data.data[0].paidOn * 1000)
+    );
+    if (nowDate.diff(lastPayoutDate, "minutes") <= 3) {
+      console.log("last payout less than 3 minutes ago");
+      TELEGRAM_BOT.telegram.sendMessage(
+        TELEGRAM_CHAT_ID,
+        "Last payout less than 3 minutes ago (" +
+          payoutsResp.data.data[0].amount / ONE_ETH_IN_WEI +
+          "ETH)"
+      );
+      TELEGRAM_BOT.telegram.sendAnimation(
+        TELEGRAM_CHAT_ID,
+        "https://media.giphy.com/media/5e22CwMaD4oMSk3Qpc/giphy.gif"
+      );
+    }
   });
-
-axios
-  .get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/dashboard`)
-  .then((res) => {
-    console.log(res.data.data.currentStatistics);
-  })
-  .catch((error) => {
-    console.error(error);
-  }); */
-
-  console.log(process.env.TELEGRAM_BOT_TOKEN);
-
-Promise.all([
-  axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/dashboard`),
-  axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/workers`),
-  axios.get(`${ETHERMINE_API}/miner/${ETH_WALLET_ADDREESS}/payouts`),
-]).then(([dashboardResp, workersResp, payoutsResp]) => {
-  console.log(dashboardResp.data.data.currentStatistics);
-  console.log('____________________');
-  console.log(workersResp.data.data);
-  console.log('____________________');
-  // console.log(payoutsResp.data.data[0].amount / ONE_ETH_IN_WEI);
-  console.log(payoutsResp.data.data);
-});
+}, 180000);
